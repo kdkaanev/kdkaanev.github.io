c7d467d87dd4aeead5b41ad5e5c470503373f695
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-10 16:45:05 +0200

    up

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,11 +2,11 @@ function solve(){
     BASE_URL = 'https://api.github.com/users/'
     const searchBtn = document.querySelector('#js-search')
     const searchUser = document.querySelector('#js-user')
-    const avatar = document.querySelector('#avatar')
-    const name = document.querySelector('#js-name')
-    const login = document.querySelector('#js-login')
-    const bio = document.querySelector('#js-bio')
-    const joined = document.querySelector('#js-joined')
+    const avatar = document.querySelector('#js-avatar')
+    const nameUser = document.querySelector('#js-name')
+    const loginUser = document.querySelector('#js-login')
+    const bioUser = document.querySelector('#js-bio')
+    const joinedUser = document.querySelector('#js-joined')
     const repos = document.querySelector('#repos')
     const folls = document.querySelector('#fols')
     const follg = document.querySelector('#follg')
@@ -14,6 +14,7 @@ function solve(){
     const web = document.querySelector('#js-web')
     const twitt = document.querySelector('#js-twitter')
     const company = document.querySelector('#js-company')
+    const noUser = document.querySelector('#err')
     searchBtn.addEventListener('click', searchHandler)
 
 
@@ -23,7 +24,46 @@ function solve(){
             .then((resp) =>resp.json())
             .then((data) =>{
                 console.log(data)
+               if(data.message === 'Not Found'){
+                noUser.textContent = 'No results'
+               }
+                let{avatar_url,
+                    bio,
+                    name,
+                    login,
+                    created_at,
+                    public_repos,
+                    followers,
+                    following,
+                    location,
+                    blog,
+                    twitter_username,
+                    company
+                } = data
+                avatar.setAttribute('src', avatar_url)
+                nameUser.textContent = showName(name, login)
+                loginUser.textContent = '@' + login
+                biography(bio, bioUser)
+                console.log(created_at)
+               
+              
             })
+            .catch(err => console.log(err.message))
+            
     }
-}
+    function showName(userName, loginName){
+        if(userName === null){
+            return loginName
+        }
+        return userName
+    }
+    function biography(b,user){
+        if(b !== null){
+            user.textContent = b
+            user.style.opacity = 1
+        }
+    }
+ 
+        
+    }
 solve()
