0ac463bff6a520c361db6ba8cabec0217bba5b3b
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2024-03-01 19:59:31 +0200

    Delete exam directory

--- exam/exam_app/exam_app/user_profile/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_name(value):
-    for char in value:
-        if not (char.isalpha() or char.isdigit() or char == '_'):
-            raise ValidationError("The string is not valid.")
